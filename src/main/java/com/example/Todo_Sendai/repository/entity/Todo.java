fe50c4c1ca4ae40612b193f72e5749621168e564
haruto_yamamoto <yamamoto.haruto@alh.inc.jp>
2026-06-25 14:05:10 +0900

    status

--- src/main/java/com/example/Todo_Sendai/repository/entity/Todo.java
+++ src/main/java/com/example/Todo_Sendai/repository/entity/Todo.java
@@ -1,5 +1,32 @@
 package com.example.Todo_Sendai.repository.entity;
 
-public class Todo {
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "todo")
+@Data
+public class Todo {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String content;
+
+    @Column(name = "updateDate")
+    private LocalDateTime updateDate;
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updateDate = LocalDateTime.now();
+    }
+    @Column(name = "limitDate")
+    private LocalDateTime limitDate;
+    @Column
+    private Integer status;
 }
