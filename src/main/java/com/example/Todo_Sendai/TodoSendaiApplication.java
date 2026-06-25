0e88612631399d07b2bd789877e0dc5a33dc30ab
konda_yuto <konda.yuto@alhinc.jp>
2026-06-25 09:38:15 +0900

    First Commit

---
+++ src/main/java/com/example/Todo_Sendai/TodoSendaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Todo_Sendai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSendaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSendaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Todo_Sendai/TodoSendaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Todo_Sendai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSendaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
